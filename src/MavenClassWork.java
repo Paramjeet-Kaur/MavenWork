5da6d1046e6b7a9908d8a72f7735cd34ae9e14e9
Parneet kaur Jattana <Parneet kaur Jattana@DESKTOP-S01EL89>
2019-07-12 11:11:52 -0400

    Add imports

--- src/MavenClassWork.java
+++ src/MavenClassWork.java
@@ -1,5 +1,19 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class MavenClassWork extends Application {
 
